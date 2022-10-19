2da35f8131660e0732e305aca4651d3ed99d28f1
Cvanimschoot <clayton.vanimschoot@gmail.com>
2022-10-19 17:39:28 -0600

    Fixed input.js. Added constants.js. Implemented constants across the different javascript files.

--- input.js
+++ input.js
@@ -1,6 +1,12 @@
+const { MOVE_UP_KEY, MOVE_DOWN_KEY, MOVE_LEFT_KEY, MOVE_RIGHT_KEY} = require("./constants");
+
+// Stores the active TCP connection object.
+let connection;
+
 // setup interface to handle user input from stdin
 
-const setupInput = function () {
+const setupInput = (conn) => {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
@@ -12,6 +18,27 @@ const setupInput = function () {
 const handleUserInput = function (key) {
   if ( key === '\u0003' ) {
     process.exit();
+  }
+  if (key === MOVE_UP_KEY) {
+    connection.write('Move: up');
+  }
+  if (key === MOVE_DOWN_KEY) {
+    connection.write('Move: down');
+  }
+  if (key === MOVE_LEFT_KEY) {
+    connection.write('Move: left');
+  }
+  if (key === MOVE_RIGHT_KEY) {
+    connection.write('Move: right');
+  }
+  if (key === '1') {
+    connection.write('Say: You suck!');
+  }
+  if (key === '2') {
+    connection.write('Say: I\'m better!');
+  }
+  if (key === '3') {
+    connection.write('Say: Sucker');
   }
 };
 
